98c7bf5db471ad29005051c2de76aa1c8d9aece5
stefane.orichuela.021 <stefane.orichuela.021@imd.local>
2023-05-25 12:55:06 -0300

    criando botao separado #1

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    TextInput,
+    Alert
+  } from 'react-native';
+  
+function Button({onPress}) {
+    return( 
+        <TouchableOpacity style={styles.button} 
+            onPress={onPress}
+        >
+            <Text style={{fontSize:20}}>CONTINUAR</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 50,
+        backgroundColor: '#f09c35',
+        width: '100%',
+        fontFamily: 'Arial',
+    },
+});
+
+export default Button;
--- src/pages/page1.tsx
+++ src/pages/page1.tsx
@@ -7,6 +7,8 @@
 
 import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
+
+
 import {
   StyleSheet,
   Text,
@@ -15,10 +17,12 @@ import {
   TextInput,
   Alert
 } from 'react-native';
+import Button from '../components/Button';
 
 function Page1({ navigation }) {
 
     const [email, setEmail] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
     const validateEmail = () => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -26,50 +30,66 @@ function Page1({ navigation }) {
     };
 
     const handleSubmit = () => {
-        Alert.alert('Texto digitado:', email);
         if (validateEmail()) {
             navigation.navigate('Page2', { propKey: email })
         } else {
-            Alert.alert('E-mail inválido');
+            setIsValid(false)
         }
      };
 
     return (
             <View
-                style={{
-                flex:1,
-                alignItems: 'center',
-                backgroundColor: 'white'
-            }}>
-                <Text style={styles.title}>E-mail</Text>
+                style={styles.container}>
+                <Text style={styles.title}>Crie sua própria conta</Text>
                 <TextInput 
                     onChangeText={(value) => setEmail(value)}
                     value={email}
+                    style={styles.input}
                     placeholder="Digite seu e-mail"
+                    placeholderTextColor="#7c8bbf"
                 />
-                <TouchableOpacity style={styles.button}
-                    onPress={handleSubmit}
-                >
-                    <Text style={{fontSize:20}}>continuar</Text>
-                </TouchableOpacity>
+               
+                  <Button onPress={handleSubmit} />
+                  {
+                    isValid ? false : (
+                      <View>
+                        <Text>Por favor, digite um e-mail válido</Text>
+                      </View>
+                    )
+                  }
+               
             </View>
     );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
   button: {
     padding: 10,
     justifyContent: 'center',
     alignItems: 'center',
-    borderColor: 'black',
-    borderWidth: 1,
+     borderRadius: 50,
+     backgroundColor: '#f09c35',
+     width: '100%',
+     fontFamily: 'Arial',
   },
-  title: {
-    fontSize: 40
+  container : {
+    flex:1,
+    alignItems: 'center',
+    backgroundColor: '#05154f',
+    padding: 20,
+    justifyContent: 'space-between',
+  },
+   title: {
+    fontSize: 30,
+    color: 'white',
+    fontFamily: 'Arial',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#7c8bbf',
+    padding: 6,
+    borderRadius: 30,
+     width: '100%',
   }
 });
 
